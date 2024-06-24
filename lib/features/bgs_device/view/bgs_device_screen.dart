2a98f818b2d1c2789660795cc8b1e1ef0fa5d79b
grig <grig_p@mail.ru>
2024-06-24 17:35:09 +0300

    Работа над вьюхой

--- lib/features/bgs_device/view/bgs_device_screen.dart
+++ lib/features/bgs_device/view/bgs_device_screen.dart
@@ -1,3 +1,5 @@
+//import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
+
 import 'package:bgs_tester/repositories/bgs_devices/bgs_devices.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,85 +12,204 @@ class BgsDeviceScreen extends StatefulWidget {
 }
 
 class _BgsDeviceScreenState extends State<BgsDeviceScreen> {
-  //String? bgsName;
   BgsInfo? bgs;
-  int _startPower = 0;
+  late TextEditingController _textCtrlName;
+  late TextEditingController _textCtrlDecription;
 
   @override
   void didChangeDependencies() {
     final args = ModalRoute.of(context)?.settings.arguments;
     assert(args != null && args is BgsInfo, 'Needed BgsInfo args');
-//    assert(args != null && args is String, 'Needed string args');
-    // bgsName = args as String;
     bgs = args as BgsInfo;
-    _startPower = (bgs as BgsInfo).startPower!;
+    _textCtrlName = TextEditingController(text: (bgs as BgsInfo).name ?? '');
+    _textCtrlDecription = TextEditingController(text: (bgs as BgsInfo).description ?? '');
 
     super.didChangeDependencies();
   }
 
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+
+  @override
+  void dispose() {
+    print('name = ${(bgs as BgsInfo).name ?? ''}');
+    print('description = ${(bgs as BgsInfo).description ?? ''}');
+    print('start power = ${((bgs as BgsInfo).startPower ?? 1)}');
+    print('power = ${((bgs as BgsInfo).power ?? 10)}');
+    print('am = ${(bgs as BgsInfo).am ?? false}');
+    print('fm = ${(bgs as BgsInfo).fm ?? false}');
+    _textCtrlName.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style =  ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20),
+    );
 
     return Scaffold(
-      appBar: AppBar(title: Text((bgs as BgsInfo).name ?? '...')),
+      appBar: AppBar(
+        title: Text((bgs as BgsInfo).name ?? '...'),
+      ),
       body: Center(
         child: Column(
           children: [
             Row(
               children: [
-                const Text('Первый параметр'),
+                const Text('О программе'),
+                const SizedBox(
+                  width: 80,
+                ),
                 ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('press me')
+                  onPressed: () {
+                    print('about');
+                    showAboutDialog(
+                      context: context,
+                      applicationName: 'bgs tester',
+                      applicationVersion: '1.0'
+                    );
+                    // const AboutDialog(
+                    //   key: Key('about'),
+                    //   applicationName: 'bgs tester',
+                    //   applicationVersion: '1.0',
+                    // );
+                  },
+                  child: const Text('press me'),
                 )
               ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text('Название'),
+                const SizedBox(
+                  width: 40,
+                ),
+                Expanded(child:
+                  CupertinoTextField(
+                    controller: _textCtrlName,
+                    onChanged: (String value) {
+                      bgs?.name = value;
+                    }
+                  )
+                ),
+                const SizedBox(
+                  width: 40,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text('Описание'),
+                const SizedBox(
+                  width: 40,
+                ),
+                Expanded(child:
+                  CupertinoTextField(
+                    controller: _textCtrlDecription,
+                    onChanged: (String value) {
+                      bgs?.description = value;
+                    },
+                  )
+                ),
+                const SizedBox(
+                  width: 40,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
             ),
             Row(
               children: [
                 const Text('Начальная мощность : '),
-                Text('$_startPower'),
-                CupertinoSlider(
-                  key: const Key('slider'),
-                  value: _startPower as double,//(bgs as BgsInfo).startPower! as double,
-                  min: 1,
-                  max: 5,
-//                  divisions: 1,
-                  onChanged: (double value) {
-                    setState(() {
-                      print(value as int);
-                      _startPower = value as int;
-                      //(bgs as BgsInfo).startPower = value as int?;
-
-                    });
-                  }
+                Text('${((bgs as BgsInfo).startPower ?? 1)}'),
+                Expanded(
+                  child: CupertinoSlider(
+                    key: const Key('startPower'),
+                    value: ((bgs as BgsInfo).startPower ?? 1).toDouble(),
+                    min: 1,
+                    max: 5,
+                    divisions: 4,
+                    onChanged: (double value) {
+                      setState(() {
+                        bgs?.startPower = (value ?? 1.0).toInt();
+                      });
+                    },
+                  ),
                 )
               ],
+            ),
+            const SizedBox(
+              height: 20,
             ),
             Row(
               children: [
-                const Text('Третий параметр'),
-                CupertinoSwitch(
-                    value: true,
-                    activeColor: CupertinoColors.activeBlue,
-                    onChanged: (bool? value) {
+                const Text('Мощность : '),
+                Text('${((bgs as BgsInfo).power ?? 10)}'),
+                Expanded(
+                  child: CupertinoSlider(
+                    key: const Key('power'),
+                    value: ((bgs as BgsInfo).power ?? 10).toDouble(),
+                    min: 1,
+                    max: 100,
+                    divisions: 99,
+                    onChanged: (double value) {
                       setState(() {
-
+                        bgs?.power = (value ?? 10).toInt();
                       });
-                    }
+                    },
+                  ),
                 )
               ],
             ),
-            // Row(
-            //   children: [
-            //     const Text('Четвертый параметр'),
-            //     CupertinoTextField(
-            //
-            //     )
-            //   ],
-            // )
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text('AM'),
+                CupertinoSwitch(
+                  value: (bgs as BgsInfo).am ?? false,
+                  activeColor: CupertinoColors.activeBlue,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      bgs?.am = value ?? false;
+                    });
+                  },
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text('FM'),
+                CupertinoSwitch(
+                  value: (bgs as BgsInfo).fm ?? false,
+                  activeColor: CupertinoColors.activeBlue,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      bgs?.fm = value ?? false;
+                    });
+                  },
+                )
+              ],
+            ),
           ],
-        )
+        ),
       ),
     );
   }
